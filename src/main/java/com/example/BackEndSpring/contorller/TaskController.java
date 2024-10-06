92e3fb58c5df5577e55bac461627ad9b2588eb21
Raghavendra Karanth <raghavendrakaranth39@gmail.com>
2024-10-06 11:07:01 +0530

    delete task added to controller and new TaskTrackerService class added

--- src/main/java/com/example/BackEndSpring/contorller/TaskController.java
+++ src/main/java/com/example/BackEndSpring/contorller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.BackEndSpring.contorller;
 
 import com.example.BackEndSpring.model.TaskTracker;
 import com.example.BackEndSpring.repository.TaskRepository;
+import com.example.BackEndSpring.service.TaskTrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,28 +17,30 @@ public class TaskController {
     @Autowired
     TaskRepository taskRepository;
 
+    @Autowired
+    TaskTrackerService taskTrackerService;
+
+
     @GetMapping("/getAllTasks")
     public ResponseEntity<List<TaskTracker>> getAllTasks() {
         try {
-            List<TaskTracker> taskTrackerList = new ArrayList<>();
-            taskRepository.findAll().forEach(taskTrackerList::add);
-
-            if (taskTrackerList.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(taskTrackerList, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            return taskTrackerService.getAllTasks();
+        } catch (Exception e) { return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
     }
 
     @PostMapping("/addTask")
     public ResponseEntity<TaskTracker> addTask(@RequestBody TaskTracker taskTracker) {
         try {
-            TaskTracker taskTrackerObj = taskRepository.save(taskTracker);
+            TaskTracker taskTrackerObj = taskTrackerService.addTask(taskTracker);
             return new ResponseEntity<>(taskTrackerObj, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        } catch (Exception e) { return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
+    }
+
+    @DeleteMapping("/deleteTaskById/{id}")
+    public ResponseEntity<HttpStatus> deleteTaskById(@PathVariable("id") Long id) {
+        try {
+            taskTrackerService.deleteTaskById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) { return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
     }
 }
